f366c7a28eeb42143015d34316dcb0f1c9be1689
jeff-gitcode <jeff.gitcode@gmail.com>
2023-11-18 00:00:39 +1100

    updated tests

--- Infrastructure.Tests/CosmosDB/UserRepositoryTests.cs
+++ Infrastructure.Tests/CosmosDB/UserRepositoryTests.cs
@@ -1,4 +1,4 @@
-﻿using Application.Users;
+﻿using AutoFixture;
 using AutoFixture.Xunit2;
 using Domain;
 using FluentAssertions;
@@ -11,38 +11,49 @@ namespace Infrastructure.Tests.CosmosDB
 {
     public class UserRepositoryTests
     {
-        private readonly Mock<Database> _database;
         private readonly Mock<Container> _container;
         private readonly Mock<IOptionsMonitor<CosmosDbOptions>> _cosmosDbOptions;
         private readonly Mock<CosmosClient> _cosmosClient;
 
         private readonly UserRepository _sut;
 
-        public UserRepositoryTests() {
-            _database = new Mock<Database>();
+        public UserRepositoryTests()
+        {
+            var fixture = new Fixture();
+            var dbOptions = fixture.Create<CosmosDbOptions>();
             _container = new Mock<Container>();
             _cosmosDbOptions = new Mock<IOptionsMonitor<CosmosDbOptions>>();
+            _cosmosDbOptions.Setup(r => r.CurrentValue).Returns(dbOptions);
             _cosmosClient = new Mock<CosmosClient>();
 
-            _sut = new UserRepository(_cosmosClient.Object, _cosmosDbOptions.Object);        
+            _cosmosClient
+                .Setup(r => r.GetDatabase(It.IsAny<string>()).GetContainer(It.IsAny<string>()))
+                .Returns(_container.Object);
+
+            _sut = new UserRepository(_cosmosClient.Object, _cosmosDbOptions.Object);
         }
 
         [Theory, AutoData]
-        public async Task UserRepositoryTests_WhenAddItemAsync_ShouldReturns(
-            Customer customer,
-            ItemResponse<Customer> response
-        )
+        public async Task UserRepositoryTests_WhenAddItemAsync_ShouldReturns(Customer customer)
         {
-            _cosmosClient.Setup(r => r.GetDatabase(It.IsAny<string>())).Returns(_database.Object);
-            
-            _cosmosClient.Setup(r => r.GetContainer(It.IsAny<string>(), It.IsAny<string>())).Returns(_container.Object);
+            Mock<ItemResponse<Customer>> itemResponse = new Mock<ItemResponse<Customer>>();
+            itemResponse.Setup(_ => _.Resource).Returns(customer);
 
-            _container.Setup(r => r.CreateItemAsync<Customer>(It.IsAny<Customer>(), It.IsAny<PartitionKey>(), null, default)).ReturnsAsync(response);
+            _container
+                .Setup(
+                    r =>
+                        r.CreateItemAsync<Customer>(
+                            It.IsAny<Customer>(),
+                            It.IsAny<PartitionKey>(),
+                            It.IsAny<ItemRequestOptions>(),
+                            It.IsAny<CancellationToken>()
+                        )
+                )
+                .ReturnsAsync(itemResponse.Object);
 
             var actual = await _sut.AddItemAsync(customer, default);
 
             actual.Should().BeEquivalentTo(customer);
         }
-
     }
 }
